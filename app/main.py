5b4ec98efe523fa173bb4e5eeb20ef40eefc8ac9
Rayan Farhan <rayanfarhan1902@gmail.com>
2025-10-15 05:44:25 +0500

    Updated cors for frontend

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # You can restrict this later to frontend URL (e.g., http://localhost:5173)
+    allow_origins=["http://localhost:5173"],  # You can restrict this later to frontend URL (e.g., http://localhost:5173)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
